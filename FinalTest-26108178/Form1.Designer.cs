5642c92a14fd40aca9257a2411df48cd90095b69
mario <mario@DESKTOP-OIDI4OL>
2022-03-19 10:22:12 -0500

    Add project files.

---
+++ FinalTest-26108178/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace FinalTest_26108178
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.titleListBox = new System.Windows.Forms.ListBox();
+            this.priceListBox = new System.Windows.Forms.ListBox();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.subTotalLabel = new System.Windows.Forms.Label();
+            this.totalWithTaxLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Price:";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(99, 12);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(100, 20);
+            this.titleTextBox.TabIndex = 2;
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(99, 45);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.priceTextBox.TabIndex = 3;
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(62, 83);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(137, 23);
+            this.enterButton.TabIndex = 4;
+            this.enterButton.Text = "Enter Title and Price";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Titles:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(165, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Prices:";
+            // 
+            // titleListBox
+            // 
+            this.titleListBox.FormattingEnabled = true;
+            this.titleListBox.Location = new System.Drawing.Point(13, 147);
+            this.titleListBox.Name = "titleListBox";
+            this.titleListBox.Size = new System.Drawing.Size(120, 95);
+            this.titleListBox.TabIndex = 7;
+            // 
+            // priceListBox
+            // 
+            this.priceListBox.FormattingEnabled = true;
+            this.priceListBox.Location = new System.Drawing.Point(139, 147);
+            this.priceListBox.Name = "priceListBox";
+            this.priceListBox.Size = new System.Drawing.Size(120, 95);
+            this.priceListBox.TabIndex = 8;
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(13, 248);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(246, 23);
+            this.displayButton.TabIndex = 9;
+            this.displayButton.Text = "Display Sub-Total and Total with 10% Sales Tax";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Sub-Total:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 316);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Total with 10% Sales Tax:";
+            // 
+            // subTotalLabel
+            // 
+            this.subTotalLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.subTotalLabel.Location = new System.Drawing.Point(145, 280);
+            this.subTotalLabel.Name = "subTotalLabel";
+            this.subTotalLabel.Size = new System.Drawing.Size(100, 23);
+            this.subTotalLabel.TabIndex = 12;
+            this.subTotalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // totalWithTaxLabel
+            // 
+            this.totalWithTaxLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalWithTaxLabel.Location = new System.Drawing.Point(145, 311);
+            this.totalWithTaxLabel.Name = "totalWithTaxLabel";
+            this.totalWithTaxLabel.Size = new System.Drawing.Size(100, 23);
+            this.totalWithTaxLabel.TabIndex = 13;
+            this.totalWithTaxLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(13, 362);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 14;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(184, 362);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 15;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.totalWithTaxLabel);
+            this.Controls.Add(this.subTotalLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.displayButton);
+            this.Controls.Add(this.priceListBox);
+            this.Controls.Add(this.titleListBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Bookstore";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox titleListBox;
+        private System.Windows.Forms.ListBox priceListBox;
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label subTotalLabel;
+        private System.Windows.Forms.Label totalWithTaxLabel;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ FinalTest-26108178/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalTest_26108178
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            // Declare Variables
+            string title;
+            double price;
+
+            // Input Data
+
+            if (double.TryParse(priceTextBox.Text, out price) && price >= 0.0 && price <= 50.0)
+            {
+                title = titleTextBox.Text;
+
+                // Display in listboxes
+                titleListBox.Items.Add(title);
+                priceListBox.Items.Add(price);
+            }
+            else
+            {
+                MessageBox.Show("Please enter price between $0.00 and $50.00");
+            }
+        }
+
+        private void displayButton_Click(object sender, EventArgs e)
+        {
+            // Declare Variables
+            int count;
+            int index;
+            string priceString;
+            double subTotal = 0;
+            double totalWithTax;
+
+            count = priceListBox.Items.Count;
+
+            if (count > 0)
+            {
+                index = 0;
+
+                do
+                {
+
+                    priceString = priceListBox.Items[index].ToString();
+
+                    subTotal += double.Parse(priceString); // total = total + profitAmountString
+
+                    index++;
+
+                } while (index < count);
+
+                totalWithTax = subTotal + (subTotal * 0.1);
+
+                // Display subTotal and totalWithTax
+                subTotalLabel.Text = subTotal.ToString("c");
+                totalWithTaxLabel.Text = totalWithTax.ToString("c");
+            }
+            else
+            {
+                MessageBox.Show("Please enter data");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            titleTextBox.Clear();
+            priceTextBox.Clear();
+            subTotalLabel.Text = String.Empty;
+            totalWithTaxLabel.Text = String.Empty;
+            titleListBox.Items.Clear();
+            priceListBox.Items.Clear();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
